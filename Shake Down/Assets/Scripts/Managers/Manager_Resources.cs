825aef6c13bbfe24aa2f867a724b399ca3ca5fab
AlbinoCerberus <albinocerberus@gmail.com>
2015-07-09 10:43:01 -0700

    Resource Scripts
    Committing resource scripts from July 7th

---
+++ Shake Down/Assets/Scripts/Managers/Manager_Resources.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Manager_Resources : MonoBehaviour 
+{
+	// Master Dictionary of Resource instances
+	static private Dictionary<string, Resources_Master> referenceIDs = new Dictionary<string, Resources_Master>();
+
+	public void ClearAllResources()
+	{
+		referenceIDs = new Dictionary<string, Resources_Master> ();
+		_shopkeepers = new Dictionary<string, Resources_Shopkeeper> ();
+		_officers = new Dictionary<string, Resources_Officer> ();
+		_player = null;
+	}
+
+	// Player
+	[SerializeField] static private Resources_Player _player;
+	static public Resources_Player player { get { return _player; } }
+	static public void NewPlayer (Resources_Player value)
+	{
+		if (IDIsUnique (value.referenceID)) {
+			_player = value; 
+			referenceIDs.Add (value.referenceID, value);
+		} else {
+			Debug.LogError ("Attempted to create a player when a player already exists"); 
+		}
+	}
+
+	// Shopkeepers
+	[SerializeField] static private Dictionary<string, Resources_Shopkeeper> _shopkeepers = new Dictionary<string, Resources_Shopkeeper>();
+	static public Dictionary<string, Resources_Shopkeeper> shopkeepers { get { return _shopkeepers; } }
+	static public void NewShopkeeper (Resources_Shopkeeper value) 
+	{
+		if (IDIsUnique (value.referenceID)) {
+			_shopkeepers.Add (value.referenceID, value);
+			referenceIDs.Add (value.referenceID, value);
+		} else {
+			Debug.LogError ("Attempted to create a shopkeeper with the existing ID '" + value.referenceID + "'.");
+		}
+	} 
+
+	// Officers
+	[SerializeField] static private Dictionary<string, Resources_Officer> _officers = new Dictionary<string, Resources_Officer>();
+	static public Dictionary<string, Resources_Officer> officers { get { return _officers; } }
+	static public void NewOfficer (Resources_Officer value)
+	{
+		if (IDIsUnique (value.referenceID)) {
+			_officers.Add (value.referenceID, value);
+			referenceIDs.Add (value.referenceID, value);
+		} else {
+			Debug.LogError ("Attempted to create an officer with the existing ID '" + value.referenceID + "'.");
+		}
+	}
+
+	static private bool IDIsUnique(string value)
+	{	if (referenceIDs.ContainsKey (value)) 
+		{	return false;
+		}	return true;
+	}
+
+	private void Update()
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			Debug.Log ("Click");
+			Scenario_01 test = new Scenario_01 ();
+			foreach(KeyValuePair<string, Resources_Master> entry in referenceIDs)
+			{
+				Debug.Log (entry);
+			}
+		}
+	}
+}
---
+++ Shake Down/Assets/Scripts/Resources/Resources_Shopkeeper.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Resources_Shopkeeper : Resources_Master
+{
+	public Resources_Shopkeeper (string newID, int startingStrength, int startingRespect, int startingFear, int startingMoney, int startingIncome, int startingExpenses) : base (newID)
+	{
+		_strength = startingStrength;
+		_respect = startingRespect;
+		_fear = startingFear;
+		_money = startingMoney;
+		_income = startingIncome;
+		_expenses = startingExpenses;
+		Manager_Resources.NewShopkeeper(this);
+	}
+
+	// Strength
+	private const uint maxStrength = 100;
+	[SerializeField] private int _strength;
+	public int strength { get { return _strength; } }
+	public void AugmentStrength (int value)
+	{	_strength = (int)Mathf.Clamp (_strength + value, 0, maxStrength);
+		if (_strength <= 0) { DeathByLackOfResources(this); } }
+
+	// Respect
+	private const uint maxRespect = 100;
+	[SerializeField] private int _respect;
+	public int respect { get { return _respect; } }
+	public void AugmentRespect (int value)
+	{
+		_respect = (int)Mathf.Clamp (_respect + value, 0, maxRespect);
+	}
+
+	// Fear
+	private const uint maxFear = 100;
+	[SerializeField] private int _fear;
+	public int fear { get { return _fear; } }
+	public void AugmentFear (int value)
+	{
+		_fear = (int)Mathf.Clamp (_fear + value, 0, maxFear);
+	}
+
+	// Money
+	[SerializeField] private int _money;
+	public int money { get { return _money; } }
+	public bool AugmentMoney (int value) 
+	{	if (_money + value >= 0) {
+			_money += value;
+			return true;
+		} else { return false; } }
+
+	// Income
+	[SerializeField] private int _income;
+	public int income { get { return _income; } }
+	public void AugmentIncome (int value)
+	{	_income += value;
+		if (_income < 0) { _income = 0; } }
+
+	//Expenses
+	[SerializeField] private int _expenses;
+	public int expenses { get { return _expenses; } }
+	public void AugmentExpenses (int value)
+	{	_expenses += value;
+		if (_expenses < 0) {
+			_expenses = 0;
+		} }
+
+	public bool CalculateFinances ()
+	{	_money = _money + (_income - _expenses);
+		if (_money < 0) {
+			return false;
+		}
+		return true; }
+
+	public int profit { get { return _income - _expenses; } }
+}
